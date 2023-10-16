11bcf0f8f305e35dc169ee964f4c77e313f7d6f8
Laia <clementecrespo.laia@gmail.com>
2023-10-16 18:01:44 +0200

    Teoria jsx

--- App.tsx
+++ App.tsx
@@ -9,14 +9,23 @@ import React from 'react';
 import {ScrollView, StatusBar, StyleSheet, Text, View} from 'react-native';
 import {Provider as PaperDelWater} from 'react-native-paper';
 
-
+const Saluda = (nom, text) => {
+  return(
+    <View><Text style={styles.sectionTitle}>{text}</Text>
+    <Text style={styles.sectionTitle}>{nom}</Text></View>);
+}
 const App = () => {
 
+  let esticLoguejat = false;
+  const alumnes = ['Laia', 'Sergi', 'Marcel'];
+
   return (
-    <View>
+    <PaperDelWater>
       <StatusBar/>
-      <Text style={styles.sectionTitle}>We</Text>
-    </View>
+      {alumnes.map((alum, index) => {
+        return (Saluda(index+" "+alum,"Benvingut/da"));
+      })}
+    </PaperDelWater>
   );
 }
 
